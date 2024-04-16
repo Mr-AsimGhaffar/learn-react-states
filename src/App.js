9da0d06b3d2c7d4f2e4f6b3e58ef87a439821bb9
Mr-AsimGhaffar <asim.ghaffar71@gmail.com>
2024-04-16 18:41:50 +0500

    created dynamic images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+// import NecklineColor from "./images/imagea.jpg";
+// import WithoutNecklineColor from "./images/imageb.jpg";
 
 function App() {
+  const neckline = require(`./images/imagea.png`);
+  const withoutNeckline = require(`./images/imageb.png`);
+  const longSleeves = require(`./images/imagec.png`);
+  const shortLength = require(`./images/imaged.png`);
+
+  const shirts = { neckline, withoutNeckline, longSleeves, shortLength };
+
+  // const [changeNecklace, setChangeNecklace] = useState(shirts.neckline);
+
+  const [imageSrc, setImageSrc] = useState(shirts.neckline);
+
+  const changeImageSrc = () => {
+    setImageSrc(shirts.withoutNeckline);
+  };
+  const changeSecondImageSrc = () => {
+    setImageSrc(shirts.neckline);
+  };
+  const changethirdImageSrc = () => {
+    setImageSrc(shirts.longSleeves);
+  };
+  const changefourthImageSrc = () => {
+    setImageSrc(shirts.shortLength);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* <img id="animation" alt="shirt" src={changeNecklace} />
+      <br />
+      <button
+        className="animate"
+        onClick={() => {
+          setChangeNecklace(shirts.withoutNeckline);
+        }}
+      >
+        change style
+      </button>
+      <button onClick={() => setChangeNecklace(shirts.neckline)}>
+        change style
+      </button> */}
+      <img src={imageSrc} alt="Image" />
+      <br />
+      <button onClick={changeImageSrc}>Default</button>
+      <button onClick={changeSecondImageSrc}>Shawl Collar</button>
+      <button onClick={changethirdImageSrc}>Extra long Length</button>
+      <button onClick={changefourthImageSrc}>Short Dress Length</button>
     </div>
   );
 }
